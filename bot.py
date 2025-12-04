db30b94f00333704496e26148f116318fd419736
Caio Rigues <riguescaio@gmail.com>
2025-12-04 11:07:58 -0300

    Bot verificando os tipos de mensagem.

---
+++ bot.py
@@ -0,0 +1,62 @@
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from TOKEN import str_token
+
+class BotTelegram:
+    def __init__(self, token: str):
+        self.application = Application.builder().token(token).build()
+        self.application.add_handler(CommandHandler("start", self.start))
+        self.application.add_handler(CommandHandler("help", self.help))
+        self.application.add_handler(MessageHandler(filters.UpdateType.MESSAGE, self.main_handler))
+
+    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Send a message when the command /start is issued."""
+        user = update.effective_user
+        await update.message.reply_html(
+            rf"Hello World, Hi {user.mention_html()}!",
+            reply_markup=ForceReply(selective=True),
+        )
+
+    async def help(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Send a message when the command /help is issued."""
+        await update.message.reply_text("""
+Seja bem-vindo ao Telegram Bot do Grupo 5 da turma ENE140!
+Este é um bot capaz de processar imagens e mensagens de voz.
+Ele reconhece automaticamente quando você envia uma imagem e realizar o reconhecimento de objetos nela usando YOLO.
+Além disso, quando você enviar um áudio, ele vai transcrever tudo o que você disse!
+
+Comandos disponíveis:
+/start - Inicia a interação com o bot
+/help - Exibe esta mensagem de ajuda
+                                        """)
+    
+    async def main_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        if update.message.photo:
+            await BotImagem(self).processar_mensagem(update, context)
+            
+        elif update.message.voice:
+            await BotAudio(self).processar_mensagem(update, context)
+            
+        elif update.message.text:
+            await update.message.reply_text("Texto recebido.")
+            
+        else:
+            await update.message.reply_text("Conteúdo não listado.")
+            
+    def run(self):
+        print("BotTelegram iniciado. Esperando mensagens...")
+        self.application.run_polling(allowed_updates=Update.ALL_TYPES) #ctrl+c
+
+class BotImagem(BotTelegram):
+    def __init__(self, parentbot: BotTelegram):
+        self.parent = parentbot
+
+    async def processar_mensagem(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("Imagem recebida.")
+
+class BotAudio(BotTelegram):
+    def __init__(self, parent_bot: BotTelegram):
+            self.parent = parent_bot
+    
+    async def processar_mensagem(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("Audio (voz) recebido.")
--- main.py
+++ main.py
@@ -1,36 +1,6 @@
-import logging
-
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-from TOKEN import token
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-logging.getLogger("httpx").setLevel(logging.WARNING)
-
-logger = logging.getLogger(__name__)
-
-class BotTelegram:
-    def __init__(self):
-        pass
-
-    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        """Send a message when the command /start is issued."""
-        user = update.effective_user
-        await update.message.reply_html(
-            rf"Hello World, Hi {user.mention_html()}!",
-            reply_markup=ForceReply(selective=True),
-        )
-
-def main() -> None:
-    application = Application.builder().token(token).build()
-    app = BotTelegram()
-
-    application.add_handler(CommandHandler("start", app.start))
-
-    #Ctrl-C
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
+from bot import *
 
 if __name__ == "__main__":
-    main()
+    TOKEN = str_token
+    app = BotTelegram(TOKEN)
+    app.run()
