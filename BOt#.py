fe18234ddfea486434282441d38819a76752a00a
isa3ella <isabellazevedo03@gmail.com>
2026-01-15 19:17:03 -0300

    Update BOt#.py

--- BOt#.py
+++ BOt#.py
@@ -1,6 +1,6 @@
 from bot_audio import BotAudio
 # --- Execução do Código ---
 if __name__ == "__main__":
-    TOKEN = '8357918736:AAGKFEOhRDIs4HEso4tlBwuT3z6Vt_2LLEk'
+    TOKEN = '8235945672:AAEvnPJIiQO5zDzxsbUQ5--9rOUIBNAg7Ko'
     meu_bot = BotAudio(TOKEN)
     meu_bot.iniciar()
