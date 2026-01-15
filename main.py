cdb7715530c073cff1ea2c57adb167851e27b0d7
Douglas <douglascarvalho.oliveira@estudante.ufjf.br>
2026-01-15 19:36:26 -0300

    Corrigem o arquivo main

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from bot_telegram import BotTelegram
 
 "Aqui será rodado o código - Tenha a chave de acesso do bot Telegram em mãos"
 
-#token = input("Digite seu token (Chave de acesso feito no BOTFather)")
-#TOKEN = token
+token = input("Digite seu token (Chave de acesso feito no BOTFather)")
+TOKEN = token
 
-TOKEN = '7982411670:AAFIo5hA8evpJy6bDTTrwrwGjb5oy5fy7Bg'
 bot_instance = BotTelegram(chave_api=TOKEN)
 bot_instance.run()
